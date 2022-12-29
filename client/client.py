47c323f6b55e3675b08593c47654706aca1dd8e0
paul <paul.fisher@elsys-design.com>
2022-12-29 15:47:49 +0100

    optimization

--- client/client.py
+++ client/client.py
@@ -2,6 +2,9 @@
 from pymodbus.client import ModbusSerialClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
+
+
+
 # declaration of client and connexion
 client = ModbusSerialClient(method='rtu',port='/dev/ttyCLIENT')
 client.connect()
@@ -10,9 +13,9 @@ print(client)
 
 
 while True:
-     toSend = input("enter a string to write in holding register : ")  # enter message to send
-     toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
-     client.write_registers(40001,toSend.encode(),0x01)
-     client.write_coil(13, True,0x01)
-     client.write_coils(15, int(toSendCoils.encode()),0x01)
-client.close()
+
+    # write to slave slave id initialized at 0x01 for communication operations
+    toSend = input("enter a string to write in holding register : ")  # enter message to send
+    toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
+    client.write_registers(40001,toSend.encode(),0x01)  # write to address 40001 of holding register
+    client.write_coils(15, int(toSendCoils.encode()),0x01) #write  to address 15 coil register
