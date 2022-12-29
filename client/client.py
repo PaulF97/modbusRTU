6035a57c1f4d2ebe47b5d84cd0e0144c7e240d69
paul <paul.fisher@elsys-design.com>
2022-12-29 15:28:53 +0100

    serveur update

--- client/client.py
+++ client/client.py
@@ -1,7 +1,7 @@
 # libraries
 from pymodbus.client import ModbusSerialClient
-
-
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.constants import Endian
 # declaration of client and connexion
 client = ModbusSerialClient(method='rtu',port='/dev/ttyCLIENT')
 client.connect()
@@ -10,8 +10,9 @@ print(client)
 
 
 while True:
-     toSend = input("enter a command : ")  # enter message to send
-    #  client.write_coil(3,True)# send the message
-    #  client.write_coil(40001,int(toSend.encode())) # send the message
-     client.write_registers(40001,toSend.encode())
+     toSend = input("enter a string to write in holding register : ")  # enter message to send
+     toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
+     client.write_registers(40001,toSend.encode(),0x01)
+     client.write_coil(13, True,0x01)
+     client.write_coils(15, int(toSendCoils.encode()),0x01)
 client.close()
