f75560eaebe62f84cb793d7617159dfd13c34204
paul <paul.fisher@elsys-design.com>
2022-12-29 14:27:49 +0100

    serveur update

--- client/client.py
+++ client/client.py
@@ -3,14 +3,15 @@ from pymodbus.client import ModbusSerialClient
 
 
 # declaration of client and connexion
-client = ModbusSerialClient(method='rtu', port='/dev/ttyCLIENT', baudrate=9600)
+client = ModbusSerialClient(method='rtu',port='/dev/ttyCLIENT')
 client.connect()
 print(client)
 
 
 
 while True:
-    toSend = input("enter a command : ")  # enter message to send
-    client.write_registers(40001,toSend.encode()) # send the message
-
+     toSend = input("enter a command : ")  # enter message to send
+    #  client.write_coil(3,True)# send the message
+    #  client.write_coil(40001,int(toSend.encode())) # send the message
+     client.write_registers(40001,toSend.encode())
 client.close()
--- serveur/server2.py
+++ serveur/server2.py
@@ -1,34 +1,41 @@
-import serial
+
+# --------------------------------------------------------------------------- #
+# import the various server implementations
+# --------------------------------------------------------------------------- #
+import asyncio
+from pymodbus.server import StartAsyncSerialServer
+
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+from pymodbus.transaction import ModbusRtuFramer, ModbusBinaryFramer
+# --------------------------------------------------------------------------- #
+# configure the service logging
+# --------------------------------------------------------------------------- #
+
+import logging
+FORMAT = ('%(asctime)-15s %(threadName)-15s'
+          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 
+async def run_server():
 
-# from pymodbus.server import StartAsyncSerialServer
-from pymodbus.server import (
-    StartAsyncSerialServer,
-    StartSerialServer,
-    StartTcpServer,
-    StartTlsServer,
-    StartUdpServer,
-)
-# from pymodbus.device import ModbusDeviceIdentification
-# from pymodbus.datastore import ModbusSequentialDataBlock
-# from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+# data store of the server
+    store = ModbusSlaveContext(
+        co=ModbusSequentialDataBlock(0, [0]*100), # coils register
+        di=ModbusSequentialDataBlock(0, [0]*100), # discrete input
+        ir=ModbusSequentialDataBlock(0, [15]*100), # input register
+        hr=ModbusSequentialDataBlock(0, [16]*100) # holding
+        )
 
+    context = ModbusServerContext(slaves=store, single=True)
 
+    print("before") 
+    await StartAsyncSerialServer(method='rtu',port = '/dev/ttySERVER', baudrate=9600) # function runs in infinit loop..
 
-# store = ModbusSlaveContext(di=ModbusSequentialDataBlock(0, [17]*100),
-# co=ModbusSequentialDataBlock(0, [17]*100),
-# hr=ModbusSequentialDataBlock(0, [17]*100),
-# ir=ModbusSequentialDataBlock(0, [17]*100))
-
-
-# context = ModbusServerContext(slaves=store, single=True)
-
-# portSerialServer = serial.Serial('/dev/ttySERVER', 9600)
-# print(portSerialServer)
-
-server = StartSerialServer(method='rtu', port='/dev/ttySERVER', baudrate=9600)
-server.start()
-print(server)
-
-    
+if __name__ == "__main__":
+    asyncio.run(run_server())
