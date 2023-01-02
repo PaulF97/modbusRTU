448794374124e2629ab6f5264dc3a7b1915f1a11
paul <paul.fisher@elsys-design.com>
2023-01-02 11:12:51 +0100

    rename file

--- client/client.py
+++ client/client.py
@@ -2,11 +2,11 @@
 from pymodbus.client import ModbusSerialClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
-
+from pymodbus.transaction import ModbusRtuFramer
 
 
 # declaration of client and connexion
-client = ModbusSerialClient(method='rtu',port='/dev/ttyCLIENT')
+client = ModbusSerialClient(method='rtu', baudrate=9600,bytesize = 8,port='/dev/ttyCLIENT', timeout=1)
 client.connect()
 print(client)
 
@@ -17,5 +17,5 @@ while True:
     # write to slave slave id initialized at 0x01 for communication operations
     toSend = input("enter a string to write in holding register : ")  # enter message to send
     toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
-    client.write_registers(40001, toSend.encode(),0x01)  # write to address 40001 of holding register
+    client.write_registers(0x9c42, toSend.encode(),0x01)  # write to address 40002 of holding register
     client.write_coils(15, int(toSendCoils),0x01) #write  to address 15 coil register
--- serveur/server.py
+++ serveur/server.py
@@ -2,19 +2,19 @@
 # --------------------------------------------------------------------------- #
 # import the various server implementations
 # --------------------------------------------------------------------------- #
-import asyncio
 from pymodbus.server import StartSerialServer
 import threading
 from pymodbus.device import ModbusDeviceIdentification
-from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 
-from pymodbus.transaction import ModbusRtuFramer, ModbusBinaryFramer
+from pymodbus.transaction import ModbusRtuFramer
 # --------------------------------------------------------------------------- #
 # configure the service logging
 # --------------------------------------------------------------------------- #
 
 import logging
+
 FORMAT = ('%(asctime)-15s %(threadName)-15s'
           ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
@@ -38,11 +38,12 @@ def data():
 def run_server(context1):
     print("before")
     print("Number of threads running : " , threading.active_count()) 
-    StartSerialServer(context=context1, method='rtu',port = '/dev/ttySERVER', baudrate=9600) # function runs in infinit loop..
+    StartSerialServer(context=context1, framer=ModbusRtuFramer, method='rtu',port = '/dev/ttySERVER', baudrate=9600) # function runs in infinit loop..
 
 if __name__ == "__main__":
 
     context = data()
+
     run = threading.Thread(target=run_server, args=(context,))
     
     run.start()
