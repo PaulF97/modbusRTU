27945f77881270ae7de0a8afcc2231bdcdeea891
paul <paul.fisher@elsys-design.com>
2023-02-10 16:13:50 +0100

    improving scripts for testing RQT

--- client/client.py
+++ client/client.py
@@ -3,26 +3,54 @@ from pymodbus.client import ModbusSerialClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
 from pymodbus.transaction import ModbusRtuFramer
+from pymodbus.constants import Defaults as ModbusDefaults
 
 import logging
-
-FORMAT = ('%(asctime)-15s %(threadName)-15s'
-          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+import time
+# FORMAT = ('%(message)-15s')
+# logging.basicConfig(format=FORMAT)
+# log = logging.getLogger()
+# log.setLevel(logging.DEBUG)
 
 # declaration of client and connexion
-client = ModbusSerialClient(method='rtu', baudrate=9600,bytesize = 8,port='/dev/ttyCLIENT', timeout=1)
+
+reading = 0
+client = ModbusSerialClient(method='rtu', port = '/dev/ttyCLIENT', stopbits=1, bytesize=8, parity='N', baudrate=9600)
+
 connexion = client.connect()
 print(client)
+print("\033[92m-------------------client connected----------------------\033[0m")
+# log.debug("client started")
+
+def responseReceived(response):
+    print("connected")
+#     log.debug("server has send data")
 
 
 while True:
-
+    
     if connexion == True:
-        toSend = input("enter a string to write in holding register : ") # enter message to send
-        toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
+        print(" enter ID to get protocole ID \n enter IO to get the number of I/0 of the card \n enter PROBE what type is sond is the card")
+        question = input("------ Witch information do you want to read ----------\n")
+        # toSend = input("enter a string to write in holding register : ") # enter message to send
+        # toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
 
-        client.write_registers(49849, toSend.encode('ascii'),0x01)  # write to address 49849 of holding register
-        client.write_coils(15, toSendCoils.encode('ascii'),0x01) #write  to address 15 coil register
+        if question == "ID":
+            sending = client.write_registers(0,10,0x01)  # write to address 49849 of holding register
+            test = client.read_holding_registers(0,1,0x01)
+        elif question == "I/O":
+            sending = client.write_register(1, 20,0x01)  # write to address 49849 of holding register
+            test = client.read_holding_registers(1,1,0X01,callback=responseReceived)
+            values = test.registers
+            print(values)        
+        elif question == "PROBE":
+            sending = client.write_register(2, 2,0x01)  # write to address 49849 of holding register
+            test = client.read_holding_registers(2,1,0X01,callback=responseReceived)
+            print(test)
+        else :
+            print("you have not entered a good value")
+
+        # print(test.decode())
+        # client.write_coils(15, toSendCoils.encode('ascii'),0x01) #write  to address 15 coil register
+    else:
+        print("client not started")
--- serveur/server.py
+++ serveur/server.py
@@ -2,10 +2,13 @@
 # --------------------------------------------------------------------------- #
 from pymodbus.server import StartSerialServer
 import threading
-from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 from pymodbus.transaction import ModbusRtuFramer
 from pymodbus.device import ModbusDeviceIdentification
+from multiprocessing import Queue, Process
+import time
+
 
 # --------------------------------------------------------------------------- #
 # configure the service logging
@@ -13,39 +16,156 @@ from pymodbus.device import ModbusDeviceIdentification
 
 import logging
 
-FORMAT = ('%(asctime)-15s %(threadName)-15s'
-          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)-15s')
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+# FORMAT = ('%(message)-15s')
+# # logging.basicConfig(format=FORMAT)
+# log = logging.getLogger()
+# log.setLevel(logging.DEBUG)
 
 
+# class CallbackDataBlock(ModbusSp   try:arseDataBlock):
+#     def __init__(self, devices, queue):
+ 
+#         self.devices = devices
+#         self.queue = queue
+
+#         values = {k: 0 for k in devices.keys()}
+#         values[0xbeef] = len(values) 
+#         super(CallbackDataBlock, self).__init__(values)
+
+#     def setValues(self, address, value):
+#         super(CallbackDataBlock, self).setValues(address, value)
+#         self.queue.put((self.devices.get(address, None), value))
+
+# def read_device_map(path):
+
+#     devices = {}
+#     with open(path, 'r') as stream:
+#         for line in stream:
+#             piece = line.strip().split(',')
+#             devices[int(piece[0], 16)] = piece[1]
+#     return devices
+
+
+# def data():
+
+#     store = ModbusSlaveContext(
+#         co=ModbusSequentialDataBlock(0, [0]*100), # coils register
+#         di=ModbusSequentialDataBlock(0, [0]*100), # discrete input
+#         ir=ModbusSequentialDataBlock(0, [0]*100), # input register
+#         hr=ModbusSequentialDataBlock(0,[12]*100)
+#     )
+
+#     context = ModbusServerContext(slaves=store, single=True)
+#     return context
+
+# def rescale_value(value):
+#     s = 1 if value >= 50 else -1
+#     c = value if value < 50 else (value - 50)
+#     return s * (c * 64)
+
+# def device_writer(queue):0
+#     while True:
+#         device, value = queue.get()
+#         scaled = rescale_value(value[0])
+#         log.debug("Write(%s) = %s" % (device, value))
+#         if not device: continue
+
+
+# def updating_writer(a):
+#     """ A worker process that runs every so often and
+#     updates live values of the context. It should be noted
+#     that there is a race condition for the update.
+
+#     :param arguments: The input arguments to the call
+#     """
+#     log.debug("updating the context")
+#     context = a[0]
+#     register = 3
+#     slave_id = 0x00
+#     address = 0x10
+#     values = context[slave_id].getValues(register, address, count=5)
+#     values = [v + 1 for v in values]
+#     log.debug("new values: " + str(values))
+#     context[slave_id].setValues(register, address, values)
+
+
+class CustomDataBlock(ModbusSparseDataBlock):
+
+    def setValues(self, address, value):
+        super(CustomDataBlock, self).setValues(address, value)
+        print("the value {} has been written to address {}".format(value, address-1))
+    # def getValues(self, address, count=1):
+    #     super(CustomDataBlock, self).getValues(address, count)
+    #     print("getting the value{} from address {}".format(count, address-1))
+
+# update the values send to the server
+def updating(a):
+    context = a[0]
+    register = 0x03
+    slave_id = 0x01
+    address = 0x00
+    values = context[slave_id].getValues(register, address, count=10)
+    print(values)
+    values = [v + 1 for v in values]
+    context[slave_id].setValues(register, address, values)
+
+def counter(cont, context):
+    slave_id = 0x01
+    flag_encenderCamaras = False
+    while True:
+        register = 3    
+        address = 0
+        values = context[slave_id].getValues(register, address, count=1)
+        if values[0] == 1 and flag_encenderCamaras == False:
+            flag_encenderCamaras = True
+        elif values[0] == 2 and flag_encenderCamaras == True: 
+            flag_encenderCamaras = False
+        if flag_encenderCamaras == True:
+            address = 0x00
+            register = 10   
+            print(cont)
+            context[slave_id].setValues(register, address, [cont])
+            cont = cont + 1 
+            time.sleep(1)
 def data():
-# data store of the server
+
     store = ModbusSlaveContext(
         co=ModbusSequentialDataBlock(0, [0]*100), # coils register
         di=ModbusSequentialDataBlock(0, [0]*100), # discrete input
         ir=ModbusSequentialDataBlock(0, [0]*100), # input register
-        hr=ModbusSequentialDataBlock(0, [0]*100) # holding
-        )
+        hr=ModbusSequentialDataBlock(0,[1]*100)
+    )
 
     context = ModbusServerContext(slaves=store, single=True)
     return context
 
-def run_server(context1):
-    identity1 = ModbusDeviceIdentification()
-    identity1.VendorName = 'Pymodbus'
-    identity1.ProductCode = 'PM'
-    identity1.VendorUrl = 'https://github.com/paulhfisher/modbusRTU.git'
-    identity1.ProductName = 'Pymodbus Server'
-    identity1.ModelName = 'Pymodbus Server'
-    identity1.MajorMinorRevision = '1.5'
+def run_callback_server():
+
+    block  = CustomDataBlock([12]*100)
+    
+    slaves  = {
+        0x01: ModbusSlaveContext(di=block, co=block, hr=block, ir=block),
+    }
+    context = ModbusServerContext(slaves=slaves, single=False)
+
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'Pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'https://github.com/paulhfisher/modbusRTU.git'
+    identity.ProductName = 'Pymodbus Server'
+    identity.ModelName = 'Pymodbus Server'
+    identity.MajorMinorRevision = '1.5'
+
+
+    hilo = threading.Thread(target=counter, args=(0,context))
+    hilo.daemon = True
+    hilo.start()
 
     print("Number of threads running : " , threading.active_count())
-    
-    StartSerialServer(context = context1, identity = identity1, method='rtu', port = '/dev/ttySERVER', baudrate=9600) # function runs in infinit loop..
+    updating(a=(context,))   
+    StartSerialServer(context=context, framer=ModbusRtuFramer, identity=identity,data_block=block, port = '/dev/ttySERVER', baudrate=9600) # function runs in infinit loop..
 
 if __name__ == "__main__":
-    context = data()
-    run = threading.Thread(target=run_server, args=(context,))
-    run.start()
+
+    # context=data()
+    run_callback_server()
