74fdbb2ed42a70820e779ab644ae79520869d965
paul <paul.fisher@elsys-design.com>
2023-02-13 15:48:10 +0100

    improve client processing

--- client/client.py
+++ client/client.py
@@ -6,7 +6,6 @@ from pymodbus.transaction import ModbusRtuFramer
 from pymodbus.constants import Defaults as ModbusDefaults
 
 import logging
-import time
 # FORMAT = ('%(message)-15s')
 # logging.basicConfig(format=FORMAT)
 # log = logging.getLogger()
@@ -15,7 +14,7 @@ import time
 # declaration of client and connexion
 
 reading = 0
-client = ModbusSerialClient(method='rtu', port = '/dev/ttyCLIENT', stopbits=1, bytesize=8, parity='N', baudrate=9600,)
+client = ModbusSerialClient(method='rtu', port = '/dev/ttyCLIENT', stopbits=1, bytesize=8, parity='N', baudrate=9600)
 
 connexion = client.connect()
 print(client)
@@ -32,23 +31,31 @@ def responseReceived(response):
 while True:
     
     if connexion == True:
-        # writeOrNot = input("Do you want to write in a register ?\n")
+        writeOrNot = input("Do you want to write in a address of holding register ?\n")
 
-        # if writeOrNot == "yes":
-        #     registerType = input("in witch register you want to write ? (coils, or holding only) :\n ")
+        if writeOrNot == "yes":
+            value = input("enter the value :\n ")
+            address = input("enter the address : \n")
+            sendCustom = client.write_registers(int(address), int(value),0x01)
         
         print(" enter ID to get protocole ID \n enter IO to get the number of I/0 of the card \n enter PROBE what type is sond is the card")
         question = input("------ Witch information do you want to read ----------\n")
-        # # toSend = input("enter a string to write in holding register : ") # enter message to send
-        # # toSendCoils = input("enter a integer to write in coils: ")  # enter message to send
 
         if question == "ID":
-            ending = client.write_registers(0,1,0x01)  # write to address 49849 of holding register
-            test = client.read_holding_registers(0,10,0x01)
+            if writeOrNot =="yes":
+                print("default value will be put back")
+            sending = client.write_registers(0,1,0x01)  # write to address 49849 of holding register
+            test = client.read_holding_registers(0,1,0x01)
+            print("read from {} to {}".format(0,1+10-2))
+            print(test)
         elif question == "I/O":
+            if writeOrNot =="yes":
+                print("default value will be put back")
             sending = client.write_register(1, 2,0x01)  # write to address 49849 of holding register
             test = client.read_holding_registers(1,1,0X01)
         elif question == "PROBE":
+            if writeOrNot =="yes":
+                print("default value will be put back")
             sending = client.write_register(2, 3,0x01)  # write to address 49849 of holding register
             test = client.read_holding_registers(2,1,0X01)
         else :
--- serveur/server.py
+++ serveur/server.py
@@ -28,8 +28,10 @@ class CustomDataBlock(ModbusSparseDataBlock):
     # set values from datastore
     def setValues(self, address, value):
         super(CustomDataBlock, self).setValues(address, value)
+        print("\033[92mdata detected by server\033[0m")
         print("the value {} has been written to address {}".format(value, address-1))
     def getValues(self, address, count=1):
+        print("request the value from address {}".format(address-1))
         return super().getValues(address, count)
 
 
@@ -39,40 +41,47 @@ def updatingValues(a):
     register = 0x03
     slave_id = 0x01
     address = 0x00
-    count = 10
-    # values = context[slave_id].getValues(register, address, count)
-    # print("the value is {}".format(values))
-    # # values = [v + 1 for v in values]
-    # # context[slave_id].setValues(register, address, values)
+    count = 20
+    values = context[slave_id].getValues(register, address, count)
+    print("datablock initialized {}".format(values))
 
 def counter(cont, context):
     slave_id = 0x01
-    flag = False
+    register = 0x03    
+    addressID = 0x0
+    addressIO = 0x1
+    addressPROBE = 0x2
+    count=1
     while True:
-        register = 0x03    
-        addressID = 0
-        addressIO = 1
-        addressPROBE = 2
-        values = context[slave_id].getValues(register, addressIO, count=1)
-        print("the value is {}".format(values))
-        time.sleep(1)
-        if values == 1 and flag == False:
+        valueRequested = context[slave_id].getValues(register, addressPROBE, count)
+        print("the value of probe is {}".format(valueRequested))
+
+        valueRequestedID = context[slave_id].getValues(register, addressID, count)
+        print("the value of ID is {}".format(valueRequestedID))
+
+        valueRequestedI0= context[slave_id].getValues(register, addressIO, count)
+        print("the value of I/O is {}".format(valueRequestedI0))
+        time.sleep(3)
+        if valueRequested == 1:
+            print("it was a ID request")
             time.sleep(1)
-            flag = True
-        elif values == 2 and flag == True: 
-            flag = False
-        if flag == True:
-            address = 0x00
-            register = 1  
-            print("test",cont)
-            context[slave_id].setValues(register, address, [cont])
-            cont = cont + 1 
-            time.sleep(1)
+            # flag = True
+        elif valueRequested == 2:
+            print("it was a I/O request")
+            print("the value is {}".format(valueRequested)) 
+            # flag = False
+        # if flag == True:
+        #     address = 0x00
+        #     register = 1  
+        #     print("test",cont)
+        #     context[slave_id].setValues(register, address, [cont])
+        #     cont = cont + 1 
+        #     time.sleep(1)
 
 
 def run_callback_server():
 
-    block  = CustomDataBlock([0]*100)
+    block  = CustomDataBlock([3,1,2]*100)
     
     slaves  = {
         0x01: ModbusSlaveContext(di=block, co=block, hr=block, ir=block),
